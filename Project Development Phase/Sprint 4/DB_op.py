e2694b4dba02ab550f29e4e55c489b333eada478
nachiappan15 <nachiappan15@gmail.com>
2022-11-23 18:53:06 +0530

    sprint 4

---
+++ Project Development Phase/Sprint 4/DB_op.py
@@ -0,0 +1,61 @@
+import ibm_db
+import random
+import  string
+from hashing import *
+conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=2d46b6b4-cbf6-40eb-bbce-6251e6ba0300.bs2io90l08kqb1od8lcg.databases.appdomain.cloud;PORT=32328;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=bmd88020;PWD=wcxrFr0o8c29Ybco","", "")
+print("Connection Successful")
+print(conn)
+
+
+def signinusersdb(email,password):
+    stmt = ibm_db.exec_immediate(conn, "select password,uid,random_key,name from users1 where EMAIL='"+email+"';")
+    while ibm_db.fetch_row(stmt) != False:
+        value = ibm_db.result(stmt, 0)
+        key = ibm_db.result(stmt, 1)
+        randomid = ibm_db.result(stmt, 2)
+        dname = ibm_db.result(stmt,3)
+        print(value)
+        r = rehashing(value,password,key,randomid,dname)
+        # print(r)
+        return r
+    return '{"result":"Not Found"}'
+
+def signupusersdb(name,email,password):
+    key = ''.join(random.choices(string.ascii_lowercase +string.digits, k=25))
+    # key = ''.join(random.choices(string.ascii_lowercase +string.digits, k=25))
+    arr = hashing(password)
+    stmt = "insert into users1 (NAME, EMAIL, PASSWORD, random_key) values ('"+name+"','"+ email+"','"+ str(arr.decode()) +"','"+ key+"' );"
+    if ibm_db.exec_immediate(conn, stmt):
+         return '{"result":"Done"}'
+    return '{"result":"Not Found"}'
+
+
+
+
+def get_savedNews(id):
+    stmt = ibm_db.exec_immediate(conn, "select * from news where USERID='"+id+"';")
+    if ibm_db.fetch_row(stmt)== True:
+        # print(ibm_db.fetch_assoc(stmt))
+        value =  ibm_db.fetch_assoc(stmt)
+        article = []
+        while value != False:
+            dict = {'image_url':value["IMAGE"] , 'title': value["TITLE"] , 'link': value["LINK"]}
+            article.append(dict)
+            value = ibm_db.fetch_assoc(stmt)
+        # arrArticle = [article]
+        # print(arrArticle)
+        return article
+    return '{"result":"Not Found"}'
+
+
+def addNewsDb(id,link,title,image):
+    stmt = ibm_db.exec_immediate(conn, "select * from news where LINK='"+link+"';")
+    if ibm_db.fetch_row(stmt)== False:
+        stmt2 = "insert into news (USERID, TITLE, IMAGE, LINK) values ('"+id+"','"+title+"','"+image +"','"+link+"');"
+        if ibm_db.exec_immediate(conn, stmt2):
+             return '{"result":"Done"}'
+        return '{"result":"Already Found"}'
+    return '{"result":"Already Found"}'
+   
+
+
---
+++ Project Development Phase/Sprint 4/app.py
@@ -0,0 +1,76 @@
+# from crypt import methods
+import json
+from flask import Flask , request ,render_template , redirect , url_for
+# from flask_cors import CORS
+from DB_op import signinusersdb  ,signupusersdb  ,addNewsDb ,get_savedNews;
+
+from utils import get_latest_news , getCategory,searchNews
+
+
+app = Flask(__name__,template_folder = 'templates')
+
+
+class sessionobj:
+    def __init__(self,email):
+        self.email = email
+
+@app.route('/')
+def welcome():
+    return render_template("login.html")
+
+@app.route('/signinusers',methods=['POST'])
+def signinusers():
+    value = signinusersdb(json.loads(request.get_data())['id'],json.loads(request.get_data())['password'])
+    return value
+
+
+@app.route('/signupusers',methods=['POST'])
+def signupusers():
+    value = signupusersdb(json.loads(request.get_data())['name'],json.loads(request.get_data())['email'],json.loads(request.get_data())['password'])
+    # print(json.loads(request.get_data()))
+    return value
+
+
+@app.route('/addNews',methods=['POST'])
+def addNews():
+    value = addNewsDb(json.loads(request.get_data())['id'],json.loads(request.get_data())['newsLink'],json.loads(request.get_data())['newsTitle'],json.loads(request.get_data())['newsImage'])
+    return value
+
+@app.route('/news')
+def news_headlines():
+    news_articles = get_latest_news()
+    # print(news_articles)
+    return render_template("news.html", news_articles=news_articles)
+    # return news_articles
+
+@app.route('/category/<category>')
+def newsCategory(category):
+    news_articles = getCategory(category)
+    return render_template("news.html", news_articles=news_articles)
+
+
+@app.route('/savedNews/<id>', methods=['GET', 'POST'])
+def showSavedNews(id):
+    news_articles = get_savedNews(id)
+    # return news_articles
+    # news_articles = get_savedNews()
+    print(news_articles)
+    # return news_articles
+    return render_template("savedNews.html", news_articles=news_articles)
+
+
+@app.route('/search/<text>')
+def newsSearch(text):
+    news_articles = searchNews(text)
+    return render_template("news.html", news_articles=news_articles)
+
+@app.route('/signup')
+def signup():
+    return render_template("signup.html")
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template("dashboard.html")
+
+if __name__ == "__main__":
+    app.run(debug = True)    
