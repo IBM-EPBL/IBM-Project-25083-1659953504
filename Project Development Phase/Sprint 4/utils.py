628b14c6f199d960ba0334207f47af0e84115a0b
nachiappan15 <nachiappan15@gmail.com>
2022-11-24 00:26:22 +0530

    sprint 4

--- Project Development Phase/Sprint 4/utils.py
+++ Project Development Phase/Sprint 4/utils.py
@@ -5,14 +5,14 @@ NEWS_API_KEY = config('NEWS_API_KEY')
 
 
 def get_latest_news():
-    news_data = requests.get(f'https://newsapi.org/v2/top-headlines?country=us&apiKey={NEWS_API_KEY}').json()
+    news_data = requests.get(f'https://newsapi.org/v2//top-headlines?country=us&apiKey={NEWS_API_KEY}').json()
     return news_data['articles']
 
 def getCategory(category):
-    news_data = requests.get(f'https://newsapi.org/v2/country=us&apiKey={NEWS_API_KEY}&category={category}').json()
+    news_data = requests.get(f'https://newsapi.org/v2//top-headlines?country=us&category={category}&apiKey={NEWS_API_KEY}').json()
     return news_data['articles']
 
 
 def searchNews(text):
-    news_data = requests.get(f'https://newsapi.org/v2/q={text}&apiKey={NEWS_API_KEY}').json()
+    news_data = requests.get(f'https://newsapi.org/v2/everything?q={text}&apiKey={NEWS_API_KEY}').json()
     return news_data['articles']
