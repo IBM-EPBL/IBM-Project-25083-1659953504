b4326278768dbd6bc521b4c655805105a0f863a0
nachiappan15 <nachiappan15@gmail.com>
2022-11-25 17:50:42 +0530

    Testing folders

---
+++ Final Deliverables/Final Code/utils.py
@@ -0,0 +1,20 @@
+import requests
+from decouple import config
+NEWS_API_KEY = config('NEWS_API_KEY')
+# COUNTRY = 'in'
+
+
+def get_latest_news():
+    news_data = requests.get(f'https://newsapi.org/v2//top-headlines?country=us&apiKey={NEWS_API_KEY}').json()
+    return news_data['articles']
+
+def getCategory(category):
+    news_data = requests.get(f'https://newsapi.org/v2/top-headlines?country=us&category={category}&apiKey={NEWS_API_KEY}').json()
+    news_data = requests.get(f'https://newsapi.org/v2//top-headlines?country=us&category={category}&apiKey={NEWS_API_KEY}').json()
+    return news_data['articles']
+
+
+def searchNews(text):
+    news_data = requests.get(f'https://newsapi.org/v2/top-headlines?country=us&q={text}&apiKey={NEWS_API_KEY}').json()
+    news_data = requests.get(f'https://newsapi.org/v2/everything?q={text}&apiKey={NEWS_API_KEY}').json()
+    return news_data['articles']
